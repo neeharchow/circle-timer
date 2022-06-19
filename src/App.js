06c8bd2a0091a2b2f3fb65a858cf911961f59aa5
neeharchow <narenchaudhry@gmail.com>
2022-06-18 22:15:10 -0400

    Adds input fields to add clock

--- src/App.js
+++ src/App.js
@@ -5,102 +5,98 @@ import { useState } from "react"
 import AddClock from "./components/AddClock"
 
 function App() {
-  const colors = [
-    "rgb(15, 30, 90,  .55)",
-    "rgb(15, 50, 90,  .55)",
-    "rgb(15, 70, 90,  .55)",
-    "rgb(15, 90, 90,  .55)",
-    "rgb(15, 110, 90, .55)",
-    "rgb(15, 130, 90, .55)",
-    "rgb(15, 150, 90, .55)",
-    "rgb(15, 170, 90, .55)",
-    "rgb(15, 190, 90, .55)",
-    "rgb(15, 210, 90, .55)",
-    "rgb(15, 230, 90, .55)",
-    "rgb(15, 250, 90, .55)",
-    "rgb(35, 250, 90, .55)",
-    "rgb(55, 250, 90, .55)",
-  ]
+	const colors = [
+		"rgb(15, 30, 90,  .55)",
+		"rgb(15, 50, 90,  .55)",
+		"rgb(15, 70, 90,  .55)",
+		"rgb(15, 90, 90,  .55)",
+		"rgb(15, 110, 90, .55)",
+		"rgb(15, 130, 90, .55)",
+		"rgb(15, 150, 90, .55)",
+		"rgb(15, 170, 90, .55)",
+		"rgb(15, 190, 90, .55)",
+		"rgb(15, 210, 90, .55)",
+		"rgb(15, 230, 90, .55)",
+		"rgb(15, 250, 90, .55)",
+		"rgb(35, 250, 90, .55)",
+		"rgb(55, 250, 90, .55)",
+	]
 
-  const [taskData, setTaskData] = useState([
-    {
-      taskTitle: "Work",
-      startTime: 28800,
-    },
-    {
-      taskTitle: "Exercise",
-      startTime: 1800,
-    },
-    {
-      taskTitle: "Read",
-      startTime: 1800,
-    },
-    {
-      taskTitle: "Meditate",
-      startTime: 598,
-    },
-    {
-      taskTitle: "Work",
-      startTime: 28800,
-    },
-    {
-      taskTitle: "Exercise",
-      startTime: 1800,
-    },
-  ])
+	const [taskData, setTaskData] = useState([
+		{
+			taskTitle: "Work",
+			startTime: 28800,
+		},
+		{
+			taskTitle: "Read",
+			startTime: 1800,
+		},
+		{
+			taskTitle: "Meditate",
+			startTime: 598,
+		},
+		{
+			taskTitle: "Free Coding",
+			startTime: 28800,
+		},
+		{
+			taskTitle: "Exercise",
+			startTime: 1800,
+		},
+	])
 
-  const [activeTimer, setActiveTimer] = useState(1)
+	const [activeTimer, setActiveTimer] = useState(1)
 
-  const radius = 120
-  const center = 400
-  // todo: create a function that can calculate the radii of the circles and pass it in
+	const radius = 120
+	const center = 400
+	// todo: create a function that can calculate the radii of the circles and pass it in
 
-  return (
-    <AppContainer>
-      <CircleContainer
-        modHeight={(center * 2).toString() + "px"}
-        modWidth={(center * 2).toString() + "px"}
-      >
-        <AddClock
-          setTaskData={setTaskData}
-          timerRadius={radius}
-          boxCenter={center}
-        />
-        {taskData.map((t, index) => {
-          return (
-            <TaskClock
-              startTime={t.startTime}
-              index={index + 1}
-              count={taskData.length}
-              radius={radius}
-              center={center}
-              color={colors[index]}
-              activeTimer={activeTimer}
-              setActiveTimer={setActiveTimer}
-              taskTitle={t.taskTitle}
-            ></TaskClock>
-          )
-        })}
-      </CircleContainer>
-    </AppContainer>
-  )
+	return (
+		<AppContainer>
+			<CircleContainer
+				modHeight={(center * 2).toString() + "px"}
+				modWidth={(center * 2).toString() + "px"}
+			>
+				<AddClock
+					setTaskData={setTaskData}
+					timerRadius={radius}
+					boxCenter={center}
+				/>
+				{taskData.map((t, index) => {
+					return (
+						<TaskClock
+							startTime={t.startTime}
+							index={index + 1}
+							count={taskData.length}
+							radius={radius}
+							center={center}
+							color={colors[index]}
+							activeTimer={activeTimer}
+							setActiveTimer={setActiveTimer}
+							taskTitle={t.taskTitle}
+						></TaskClock>
+					)
+				})}
+			</CircleContainer>
+		</AppContainer>
+	)
 }
 
 export default App
 
 const AppContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  flex: 1;
-  overflow-y: scroll;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+	flex: 1;
+	overflow-y: scroll;
 `
 const CircleContainer = styled.div`
-  position: relative;
-  height: ${(props) => props.modWidth};
-  width: ${(props) => props.modHeight};
-  display: flex;
-  margin: auto auto;
-  transform: rotateZ(-90deg);
+	position: relative;
+	height: ${(props) => props.modWidth};
+	width: ${(props) => props.modHeight};
+	display: flex;
+	margin: auto auto;
+	transform: rotateZ(-90deg);
 `
--- src/components/AddClock.js
+++ src/components/AddClock.js
@@ -1,72 +1,154 @@
 import styled from "styled-components"
 import React, { useEffect, useState } from "react"
 import {
-  buildStyles,
-  CircularProgressbarWithChildren,
+	buildStyles,
+	CircularProgressbarWithChildren,
 } from "react-circular-progressbar"
 import "react-circular-progressbar/dist/styles.css"
-import CircularSlider from "@fseehawer/react-circular-slider"
-// import TextField from "@mui/material/TextField"
+// import CircularSlider from "@fseehawer/react-circular-slider"
+import TextField from "@mui/material/TextField"
+import InputLabel from "@mui/material/InputLabel"
+import InputAdornment from "@mui/material/InputAdornment"
 
 function AddClock({ setTaskData, timerRadius, boxCenter }) {
-  const size = 2 * boxCenter - 4 * timerRadius - 20
+	const size = 2 * boxCenter - 4 * timerRadius - 20
 
-  return (
-    <Clock modHeight={size.toString() + "px"} modWidth={size.toString() + "px"}>
-      <CircularProgressbarWithChildren
-        strokeWidth={7}
-        styles={buildStyles({
-          strokeLinecap: "butt",
-          trailColor: "white",
-          pathColor: "#2C699A",
-          textSize: "12",
-          textColor: "black",
-          boxShadow: "1px 1px 1px 1px gray",
-        })}
-      >
-        <div style={{ width: "84%" }}>
-          <CircularProgressbarWithChildren
-            strokeWidth={7}
-            value={70}
-            styles={buildStyles({
-              strokeLinecap: "butt",
-              trailColor: "transparent",
-              pathColor: "#0DB39E",
-            })}
-          >
-            <div style={{ width: "84%" }}>
-              <CircularProgressbarWithChildren
-                strokeWidth={7}
-                value={70}
-                styles={buildStyles({
-                  strokeLinecap: "butt",
-                  trailColor: "transparent",
-                  pathColor: "#F1C453",
-                })}
-              >
-                <div>
-                  {/* <TextField></TextField> */}
-                  <input type="number"></input>
-                  <input type="number"></input>
-                </div>
-              </CircularProgressbarWithChildren>
-            </div>
-          </CircularProgressbarWithChildren>
-        </div>
-      </CircularProgressbarWithChildren>
-    </Clock>
-  )
+	const [timerTitle, setTimerTitle] = useState("")
+	const [timerHours, setTimerHours] = useState(8)
+	const [timerMinutes, setTimerMinutes] = useState(50)
+	const [timerSeconds, setTimerSeconds] = useState(30)
+
+	return (
+		<Clock modHeight={size.toString() + "px"} modWidth={size.toString() + "px"}>
+			<CircularProgressbarWithChildren
+				strokeWidth={7}
+				value={(100 * timerHours) / 24}
+				styles={buildStyles({
+					strokeLinecap: "butt",
+					trailColor: "white",
+					pathColor: "#2C699A",
+					textSize: "12",
+					textColor: "black",
+					boxShadow: "1px 1px 1px 1px gray",
+				})}
+			>
+				<div style={{ width: "84%" }}>
+					<CircularProgressbarWithChildren
+						strokeWidth={7}
+						value={(100 * timerMinutes) / 60}
+						styles={buildStyles({
+							strokeLinecap: "butt",
+							trailColor: "transparent",
+							pathColor: "#0DB39E",
+						})}
+					>
+						<div style={{ width: "84%" }}>
+							<CircularProgressbarWithChildren
+								strokeWidth={7}
+								value={(100 * timerSeconds) / 60}
+								styles={buildStyles({
+									strokeLinecap: "butt",
+									trailColor: "transparent",
+									pathColor: "#F1C453",
+								})}
+							>
+								<AddClockInput>
+									<TextField
+										value={timerTitle}
+										onChange={(e) => {
+											setTimerTitle(e.target.value)
+										}}
+										placeholder="Task Name"
+										sx={{ m: 1, width: "24ch" }}
+										variant="outlined"
+									/>
+									<div style={{ display: "flex", flexDirection: "row" }}>
+										<TextField
+											error={timerHours > 24 || timerHours < 0 ? true : false}
+											value={timerHours}
+											onChange={(e) => {
+												setTimerHours(e.target.value)
+											}}
+											helperText="Hours"
+											id="filled-start-adornment"
+											sx={{ m: 1, width: "7ch" }}
+											type="number"
+											variant="outlined"
+											InputProps={{
+												inputProps: {
+													max: 24,
+													min: 0,
+												},
+											}}
+										/>
+										<TextField
+											error={
+												timerMinutes > 60 || timerMinutes < 0 ? true : false
+											}
+											value={timerMinutes}
+											onChange={(e) => {
+												setTimerMinutes(e.target.value)
+											}}
+											helperText="Minutes"
+											id="filled-start-adornment"
+											sx={{ m: 1, width: "7ch" }}
+											type="number"
+											variant="outlined"
+											InputProps={{
+												inputProps: {
+													max: 60,
+													min: 0,
+												},
+											}}
+										/>
+										<TextField
+											error={
+												timerSeconds > 60 || timerSeconds < 0 ? true : false
+											}
+											value={timerSeconds}
+											onChange={(e) => {
+												setTimerSeconds(e.target.value)
+											}}
+											helperText="Seconds"
+											id="filled-start-adornment"
+											sx={{ m: 1, width: "7ch" }}
+											type="number"
+											variant="outlined"
+											InputProps={{
+												inputProps: {
+													max: 60,
+													min: 0,
+												},
+											}}
+										/>
+									</div>
+								</AddClockInput>
+							</CircularProgressbarWithChildren>
+						</div>
+					</CircularProgressbarWithChildren>
+				</div>
+			</CircularProgressbarWithChildren>
+		</Clock>
+	)
 }
 
 export default AddClock
 
 const Clock = styled.div`
-  position: relative;
-  display: flex;
-  margin: auto auto;
-  transform: rotateZ(90deg);
-  // height: 200px;
-  // width: 200px;
-  // height: ${(props) => props.modHeight};
-  // width: ${(props) => props.modWidth};
+	position: relative;
+	display: flex;
+	margin: auto auto;
+	transform: rotateZ(90deg);
+	// height: 200px;
+	// width: 200px;
+	// height: ${(props) => props.modHeight};
+	// width: ${(props) => props.modWidth};
+`
+
+const AddClockInput = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	transform: scale(0.8);
 `
